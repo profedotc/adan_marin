6cf3ce0f2041717e3fbefabc08c27e7cd4afac67
adan <adan@debian>
2019-05-25 18:31:26 +0200

    Corregido ejercicio 1
    Signed-off-by: adan <adan@debian>
    
    	modified:   main.c

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define TAM_X 12
-#define TAM_Y 12
+#define TAM_X 10
+#define TAM_Y 10
 
 void gol_init(bool mundo_1[][TAM_Y]);
 void gol_print(bool mundo_1[][TAM_Y]);
@@ -52,11 +52,11 @@ void gol_init(bool mundo_1[][TAM_Y])
 	 *           . . #
 	 *           # # #
 	 */
+	mundo_1[0][1] = 1;
 	mundo_1[1][2] = 1;
-	mundo_1[2][3] = 1;
-	mundo_1[3][1] = 1;
-	mundo_1[3][2] = 1;
-	mundo_1[3][3] = 1;
+	mundo_1[2][0] = 1;
+	mundo_1[2][1] = 1;
+	mundo_1[2][2] = 1;
 }
 
 void gol_print(bool mundo_1[][TAM_Y])
@@ -74,9 +74,9 @@ void gol_print(bool mundo_1[][TAM_Y])
 	 *     . . . . . . . . . .
 	 *     . . . . . . . . . .
 	 */
-	for (int x = 1; x < TAM_X - 1; x++)
+	for (int x = 0; x < TAM_X; x++)
 	{
-		for (int y = 1; y < TAM_Y - 1; y++)
+		for (int y = 0; y < TAM_Y; y++)
 		{
 				printf("%c ", mundo_1[x][y]? '#' : '.');
 		}
@@ -98,9 +98,9 @@ void gol_step(bool mundo_1[][TAM_Y], bool mundo_2[][TAM_Y])
 	 * - Copiar el mundo auxiliar sobre el mundo principal
 	 */
 	int vecinas_vivas = 0;
-	for(int x = 1; x < TAM_X - 1; x++)
+	for(int x = 0; x < TAM_X; x++)
 	{
-		for(int y = 1; y < TAM_Y - 1; y++)
+		for(int y = 0; y < TAM_Y; y++)
 		{
 			vecinas_vivas = gol_count_neighbors(mundo_1, x, y);
 
@@ -123,19 +123,19 @@ void gol_step(bool mundo_1[][TAM_Y], bool mundo_2[][TAM_Y])
 int gol_count_neighbors(bool mundo_1[][TAM_Y], int x, int y)
 {
 	// Devuelve el nÃºmero de vecinos
+	
 	int total_vecinas_vivas = 0;
+	total_vecinas_vivas += gol_get_cell(mundo_1, x - 1, y - 1);
+	total_vecinas_vivas += gol_get_cell(mundo_1, x - 1, y);
+	total_vecinas_vivas += gol_get_cell(mundo_1, x - 1, y + 1);
 
-	total_vecinas_vivas += mundo_1[x - 1][y - 1];
-	total_vecinas_vivas += mundo_1[x - 1][y];
-	total_vecinas_vivas += mundo_1[x - 1][y + 1];
+	total_vecinas_vivas += gol_get_cell(mundo_1, x, y - 1);
+	total_vecinas_vivas += gol_get_cell(mundo_1, x, y + 1);
 
-	total_vecinas_vivas += mundo_1[x][y - 1];
-	total_vecinas_vivas += mundo_1[x][y + 1];
-
-	total_vecinas_vivas += mundo_1[x + 1][y - 1];
-	total_vecinas_vivas += mundo_1[x + 1][y];
-	total_vecinas_vivas += mundo_1[x + 1][y + 1];
-
+	total_vecinas_vivas += gol_get_cell(mundo_1, x + 1, y - 1);
+	total_vecinas_vivas += gol_get_cell(mundo_1, x + 1, y);
+	total_vecinas_vivas += gol_get_cell(mundo_1, x + 1, y + 1);
+	
 	return total_vecinas_vivas;
 }
 
@@ -145,22 +145,23 @@ bool gol_get_cell(bool mundo_1[][TAM_Y], int x, int y)
 	 * TODO: Devuelve el estado de la cÃ©lula de posiciÃ³n indicada
 	 * (Â¡cuidado con los lÃ­mites del array!)
 	 */
-	if (mundo_1[x][y])
-	{
-		return 1;
-	}
-	else
+	
+	if ( (x < 0) || (x > (TAM_X - 1)) || (y < 0) || (y > (TAM_Y - 1)) )
 	{
 		return 0;
 	}
-}
+	else
+	{
+		return mundo_1[x][y];
+	}
+}	
 
 void gol_copy(bool mundo_1[][TAM_Y], bool mundo_2[][TAM_Y])
 {
 	// TODO: copia el mundo segundo mundo sobre el primero
-	for (int x = 1; x < TAM_X - 1; x++)
+	for (int x = 0; x < TAM_X; x++)
 	{
-		for (int y = 1; y < TAM_Y - 1; y++)
+		for (int y = 0; y < TAM_Y; y++)
 		{
 			mundo_1[x][y] = mundo_2[x][y];
 		}
